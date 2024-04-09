63fca2786993f6a9472eb64c3164c22433ca050e
MANGNEK <trungnguyen.kc5@gmail.com>
2024-04-09 14:16:44 +0700

    Update Program.cs

--- SimpleWithMongo/Program.cs
+++ SimpleWithMongo/Program.cs
@@ -13,11 +13,9 @@ builder.Services.ConfigureDataBase(configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
